b8a30db59aa2e6774b021ad4d0247c9277f7844d
diego <diegogon1718@gmail.com>
2023-05-04 23:08:29 -0500

    comentarios listas

--- listas/lista.1.py
+++ listas/lista.1.py
@@ -1,14 +1,14 @@
 #[] {} ()
 x=100
-print('tipo de x',type(x))
+print('tipo de x',type(x)) #funcion devuelve el tipo del objeto especificado 
 lista=[1,1.4,'sena',['a','b',],'soacha']
 print(f'elemento {lista[4]}')
-print(len(lista))
+print(len(lista)) # esta funcion devuelve el numero de elementos en un objeto
 print('tipo de lista',type(lista))
-print('ultimo indice',lista[-1])
+print('ultimo indice',lista[-1]) #
 
 print(len(lista))
-lista.append(20)
+lista.append(20) #esta funcion añade un elemento al final de la lista
 lista.append('python')
 print(lista)
 lista.insert(len(lista),'java')
--- listas/lista2.py
+++ listas/lista2.py
@@ -1,9 +1,9 @@
-import random
-lista=[]
-tam=int(random.randint(10,20))
+import random #esta fincuion en ustilizada para generar numeros aleatoreos.
+lista=[] 
+tam=int(random.randint(10,20)) # esta funcion es un metodo que devuelve un elemento seleccionado de un numero entero del rango en especifico.
 print(tam)
-for i in range(tam):
-    num=int(random.randrange(100))
-    lista.append(num)
+for i in range(tam): 
+    num=int(random.randrange(100)) # con esta funcion se de vuelve un elemento seleccionado alatoriamente del rango especificado.
+    lista.append(num) #con esta funcion se agrega un elemento al final de lista. 
 
 print(lista)
